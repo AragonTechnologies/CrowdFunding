8152edc6ed3920ff0a94763ca2fbf4f0871e7a80
Emmanuel_umeh <40989414+Emmanuel-umeh@users.noreply.github.com>
2020-02-26 14:52:21 -0800

    Add files via upload

---
+++ client/src/views/thank.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+
+export default class ThankYou extends Component{
+    render(){
+
+        return(
+
+            <div>
+                <h1>Thank You for Signing Up</h1>
+            </div>
+        )
+    }
+}
