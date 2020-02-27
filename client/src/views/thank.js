d3622ca88210a5710afed1b117b60a6c6c6bcf4b
Goodness Ezeokafor <gootech442@yahoo.com>
2020-02-28 00:30:22 +0100

    Heroku first Deploy

--- client/src/views/thank.js
+++ client/src/views/thank.js
@@ -8,7 +8,7 @@ export default class ThankYou extends Component{
 
         return(
 <div class="jumbotron text-center">
-  <h1 class="display-3">Thank You!</h1>
+  <h1 class="display-3">Thank You! Check Your Inbox</h1>
   {/* <p class="lead"><strong>Please check your email</strong> for further instructions on how to complete your account setup.</p> */}
   <hr />
 
